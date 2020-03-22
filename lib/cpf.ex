5859f8b3196bb1178b2db70ff102737d09cf1b4d
Nilton Paiva <nfrpaiva@gmail.com>
2020-03-21 21:46:49 -0300

    Aplicando formataçao

--- lib/cpf.ex
+++ lib/cpf.ex
@@ -19,19 +19,19 @@ defmodule Cpf do
   defstruct number: Integer
 
   def new(value) when is_number(value) do
-      %Cpf{number: value}
+    %Cpf{number: value}
   end
 
   def new(value) when is_binary(value) do
-      %Cpf{number: String.to_integer(value)    }
+    %Cpf{number: String.to_integer(value)}
   end
 
   defprotocol Transform do
-      def to_list(integer)
+    def to_list(integer)
   end
 
   defimpl Cpf.Transform, for: Cpf do
-      def to_list(cpf), do: Integer.digits(cpf.number)
+    def to_list(cpf), do: Integer.digits(cpf.number)
   end
 
   def is_valid(cpf) when is_binary(cpf) do
@@ -39,7 +39,6 @@ defmodule Cpf do
   end
 
   def is_valid(cpf) when is_number(cpf) do
-
     numero = get_numero(cpf)
     digito = gerar_list_digito(numero)
     numero_e_digito = numero ++ digito
@@ -50,7 +49,6 @@ defmodule Cpf do
     else
       {:error, "Cpf é inválido", cpf}
     end
-
   end
 
   def gerar_digito(numero) when is_binary(numero) do
@@ -59,9 +57,10 @@ defmodule Cpf do
 
   def gerar_digito(numero) when is_number(numero) do
     numero = Integer.digits(numero)
-    digito =  gerar_list_digito(numero)
+    digito = gerar_list_digito(numero)
+
     Enum.map(digito, fn x -> Integer.to_string(x) end)
-      |> Enum.reduce(fn b,a -> a <> b end)
+    |> Enum.reduce(fn b, a -> a <> b end)
   end
 
   defp todos_digito_sao_iguais(list) when is_list(list) do
@@ -73,36 +72,35 @@ defmodule Cpf do
   defp gerar_list_digito(numero) when is_list(numero) do
     numero_mais_j = numero ++ [resto(numero)]
     numero_mais_k = numero_mais_j ++ [resto(numero_mais_j)]
-    {_,digito} = Enum.split(numero_mais_k, -2)
+    {_, digito} = Enum.split(numero_mais_k, -2)
     digito
   end
 
   defp get_numero(numero) when is_number(numero) do
     numero_list = Integer.digits(numero)
-    Enum.take(numero_list,length(numero_list)-2)
+    Enum.take(numero_list, length(numero_list) - 2)
   end
 
   defp list_to_integer(list) when is_list(list) do
     Enum.map(list, fn x -> Integer.to_string(x) end)
-      |> Enum.reduce(fn b,a -> a <> b end)
-      |> String.to_integer
+    |> Enum.reduce(fn b, a -> a <> b end)
+    |> String.to_integer()
   end
 
   defp resto(numero) do
     Enum.reverse(numero)
-      |> Enum.map_reduce(2, fn x, acc -> {x*acc, acc+1} end)
-      |> elem(0)
-      |> Enum.reduce(fn a,b -> a+b end)
-      |> rem(11)
-      |> digito
+    |> Enum.map_reduce(2, fn x, acc -> {x * acc, acc + 1} end)
+    |> elem(0)
+    |> Enum.reduce(fn a, b -> a + b end)
+    |> rem(11)
+    |> digito
   end
 
   defp digito(resto) do
-    if resto <=1 do
+    if resto <= 1 do
       0
     else
       11 - resto
     end
   end
-
 end
--- test/cpf_test.exs
+++ test/cpf_test.exs
@@ -1,48 +1,51 @@
 defmodule CpfTest do
-    use ExUnit.Case
-    doctest Cpf
+  use ExUnit.Case
+  doctest Cpf
 
-    test "create new cpf by integer" do
-        assert %Cpf{number: 1} == Cpf.new(1)
-    end
-    test "crate new cpf by String" do
-        assert %Cpf{number: 01} == Cpf.new("01")
-    end
+  @cpf_ok {:ok, "Válido"}
+  @cpf_nao_ok {:error, "Cpf é inválido", 28_191_575_801}
 
-    test "return cpf as list" do
-        assert [1,2,3,4] ==  Cpf.new(1234) |> Cpf.Transform.to_list
-    end
+  test "create new cpf by integer" do
+    assert %Cpf{number: 1} == Cpf.new(1)
+  end
 
-    test "cpf é valido" do
-        assert Cpf.is_valid(28191575809) == {:ok, "Válido"}
-    end
+  test "crate new cpf by String" do
+    assert %Cpf{number: 01} == Cpf.new("01")
+  end
 
-    test "cpf é valido como string" do
-        assert Cpf.is_valid("07834965085") == {:ok, "Válido"}
-    end
+  test "return cpf as list" do
+    assert [1, 2, 3, 4] == Cpf.new(1234) |> Cpf.Transform.to_list()
+  end
 
-    test "cpf não é valido com numeros iguais zeros" do
-        refute Cpf.is_valid("00000000000") == {:ok, "Válido"}
-    end
+  test "cpf é valido" do
+    assert Cpf.is_valid(28_191_575_809) == @cpf_ok
+  end
 
-    test "cpf não é valido com numeros iguais uns" do
-        refute Cpf.is_valid("11111111111") == {:ok, "Válido"}
-    end
+  test "cpf é valido como string" do
+    assert Cpf.is_valid("07834965085") == @cpf_ok
+  end
 
-    test "cpf 078.349.650-85 é válido" do
-        assert Cpf.is_valid(7834965085) == {:ok, "Válido"}
-    end
+  test "cpf não é valido com numeros iguais zeros" do
+    refute Cpf.is_valid("00000000000") == @cpf_ok
+  end
 
-    test "cpf é não é valido" do
-        refute Cpf.is_valid(28191575801) == {:ok, "Cpf é válido"}
-    end
+  test "cpf não é valido com numeros iguais uns" do
+    refute Cpf.is_valid("11111111111") == @cpf_ok
+  end
 
-    test "cpf inválido, com mensagem de erro correta" do
-        assert Cpf.is_valid(28191575801) == {:error, "Cpf é inválido", 28191575801}
-    end
+  test "cpf 078.349.650-85 é válido" do
+    assert Cpf.is_valid(7_834_965_085) == @cpf_ok
+  end
 
-    test "Gerar digito verificador" do
-        assert Cpf.gerar_digito(281915758) == "09"
-    end
+  test "cpf é não é valido" do
+    refute Cpf.is_valid(28_191_575_801) == @cpf_ok
+  end
 
+  test "cpf inválido, com mensagem de erro correta" do
+    assert Cpf.is_valid(28_191_575_801) == @cpf_nao_ok
+  end
+
+  test "Gerar digito verificador" do
+    assert Cpf.gerar_digito(281_915_758) == "09"
+  end
 end
